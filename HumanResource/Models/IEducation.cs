379238a49d3abd67ccc8abc93eb27dc1a8ba04ac
Muhammad Sibghatullah Khan <sibghatuk@gmail.com>
2015-05-14 19:59:10 -0500

    added the IPerson and IEducation Model

---
+++ HumanResource/Models/IEducation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HumanResource.Models
+{
+    public interface IEducation
+    {
+        String SchoolName { get; set; }
+        String Degree { get; set;  }
+        DateTime StartDate { get; set; }
+        DateTime? EndDate { get; set; }
+
+        String Notes { get; set; }
+    }
+}
---
+++ HumanResource/Models/IPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResource.Models
+{
+    public interface IPerson
+    {
+        String Firstnmae { get; set; }
+
+        String LastName { get; set; }
+
+        DateTime BirthDate { get; set; }
+
+        DateTime? PassedAway { get; set; }
+
+        String ExtraInfo { get; set; }
+
+        List<IEducation> Educations { get; set; }
+
+        String Notes { get; set;  }
+    }
+}
